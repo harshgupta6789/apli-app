e5229aa6f8a335aab94bea9d6463aecdc0c602a7
OjasKarmarkar <ojask2002@gmail.com>
2020-04-17 18:26:01 +0530

    Notification WIP & added Firebase messaging sdk

--- lib/Screens/Home/updates.dart
+++ lib/Screens/Home/updates.dart
@@ -1,9 +1,12 @@
+import 'dart:io';
+
 import 'package:apli/Shared/constants.dart';
 import 'package:apli/Shared/customDrawer.dart';
 import 'package:apli/Shared/loading.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 class Updates extends StatefulWidget {
@@ -12,10 +15,12 @@ class Updates extends StatefulWidget {
 }
 
 class _UpdatesState extends State<Updates> {
+  FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
   Stream notifications;
   FirebaseUser user;
   List filters = ["candidateThirteen@gmail.com"];
-  bool isNotification = false;
+  List filtersFromFirebase = [];
+  bool isNotification = true;
 
   userInit() async {
     FirebaseUser user = await FirebaseAuth.instance.currentUser();
@@ -24,20 +29,29 @@ class _UpdatesState extends State<Updates> {
 
   @override
   void initState() {
+    firebaseCloudMessaging_Listeners();
     super.initState();
     userInit();
-    notifications = Firestore.instance
-        .collection("notifications")
-        .orderBy("timestamp")
-        .snapshots();
+    notifications = Firestore.instance.collection("notifications").snapshots();
+  }
+
+  void firebaseCloudMessaging_Listeners() {
+    _firebaseMessaging.getToken().then((token) {
+      print("THIS : " + token);
+    });
   }
 
   bool isMyNotification(AsyncSnapshot x) {
     x.data.documents.forEach((f) {
-      print(f);
-      return false;
+      if (f.data['receivers'] != null) {
+        // print(List.from(f.data['receivers']));
+        filtersFromFirebase.add(List.from(f.data['receivers']));
+      }
     });
-    return false;
+
+    print(filtersFromFirebase);
+
+    return isNotification;
   }
 
   @override
@@ -77,131 +91,111 @@ class _UpdatesState extends State<Updates> {
             stream: notifications,
             builder: (context, snapshot) {
               if (snapshot.hasData) {
-                if(!isMyNotification(snapshot)) return Center(
-                  child: Text('no new updates yet'),
-                );
-                 else return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(children: <Widget>[
-                    // Row(
-                    //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    //   children: <Widget>[
-                    //     RawChip(
-                    //       label: Text("New Jobs"),
-                    //       selected: false,
-                    //       backgroundColor: basicColor,
-                    //     ),
-                    //     RawChip(label: Text("Messages"), selected: true),
-                    //     RawChip(label: Text("Application"), selected: false),
-                    //   ],
-                    // ),
-                    Container(
-                      height: 180.0,
-                      child: Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15.0),
-                        ),
-                        child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              ListTile(
-                                title: Text(
-                                    "We found 7 new jobs at Deloitte, Samsung, Infosys and 4 others that you may be interested in."),
-                                trailing: Text("8h"),
+                if (!isMyNotification(snapshot)) {
+                  return Center(
+                    child: Text('no new updates yet'),
+                  );
+                } else {
+                  return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ListView.builder(
+                          itemBuilder: (BuildContext context, int index) {
+                            return Container(
+                              height: 180.0,
+                              child: Card(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                ),
+                                child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      ListTile(
+                                        title: Text(snapshot
+                                                .data
+                                                .documents[index]
+                                                .data['message'] ??
+                                            "No Message Exception"),
+                                        trailing: Text("1 mo"),
+                                      ),
+                                      Padding(
+                                          padding: EdgeInsets.only(
+                                              top: 20.0, left: 10.0),
+                                          child: Container(
+                                            decoration: BoxDecoration(
+                                              border:
+                                                  Border.all(color: basicColor),
+                                              borderRadius:
+                                                  BorderRadius.circular(10),
+                                            ),
+                                            child: MaterialButton(
+                                                child: Text(
+                                                  "View Application",
+                                                  style: TextStyle(
+                                                      fontSize: 18.0,
+                                                      color: basicColor,
+                                                      fontWeight:
+                                                          FontWeight.w600),
+                                                ),
+                                                onPressed: () => null),
+                                          )),
+                                    ]),
                               ),
-                              Padding(
-                                  padding:
-                                      EdgeInsets.only(top: 20.0, left: 10.0),
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      border: Border.all(color: basicColor),
-                                      borderRadius: BorderRadius.circular(10),
-                                    ),
-                                    child: MaterialButton(
-                                        child: Text(
-                                          "View Jobs",
-                                          style: TextStyle(
-                                              fontSize: 18.0,
-                                              color: basicColor,
-                                              fontWeight: FontWeight.w600),
-                                        ),
-                                        onPressed: () => null),
-                                  )),
-                            ]),
-                      ),
-                    ),
-                    Container(
-                      height: 180.0,
-                      child: Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15.0),
-                        ),
-                        child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              ListTile(
-                                title: Text(
-                                    "You have been shortlisted for the job - Data Analyst at Deloitte."),
-                                trailing: Text("1 mo"),
-                              ),
-                              Padding(
-                                  padding:
-                                      EdgeInsets.only(top: 20.0, left: 10.0),
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      border: Border.all(color: basicColor),
-                                      borderRadius: BorderRadius.circular(10),
-                                    ),
-                                    child: MaterialButton(
-                                        child: Text(
-                                          "View Application",
-                                          style: TextStyle(
-                                              fontSize: 18.0,
-                                              color: basicColor,
-                                              fontWeight: FontWeight.w600),
-                                        ),
-                                        onPressed: () => null),
-                                  )),
-                            ]),
-                      ),
-                    ),
-                    Container(
-                      height: 180.0,
-                      child: Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15.0),
-                        ),
-                        child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              ListTile(
-                                title: Text(
-                                    "You have an interview at Deloitte, scheduled on 2/04/2020"),
-                                trailing: Text("1 mo"),
-                              ),
-                              Padding(
-                                  padding:
-                                      EdgeInsets.only(top: 20.0, left: 10.0),
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      border: Border.all(color: basicColor),
-                                      borderRadius: BorderRadius.circular(10),
-                                    ),
-                                    child: MaterialButton(
-                                        child: Text(
-                                          "View Application",
-                                          style: TextStyle(
-                                              fontSize: 18.0,
-                                              color: basicColor,
-                                              fontWeight: FontWeight.w600),
-                                        ),
-                                        onPressed: () => null),
-                                  )),
-                            ]),
-                      ),
-                    )
-                  ]),
-                );
+                            );
+                          },
+                          itemCount: snapshot.data.documents.length)
+
+                      // Row(
+                      //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      //   children: <Widget>[
+                      //     RawChip(
+                      //       label: Text("New Jobs"),
+                      //       selected: false,
+                      //       backgroundColor: basicColor,
+                      //     ),
+                      //     RawChip(label: Text("Messages"), selected: true),
+                      //     RawChip(label: Text("Application"), selected: false),
+                      //   ],
+                      // ),
+
+                      //
+                      // Container(
+                      //   height: 180.0,
+                      //   child: Card(
+                      //     shape: RoundedRectangleBorder(
+                      //       borderRadius: BorderRadius.circular(15.0),
+                      //     ),
+                      //     child: Column(
+                      //         crossAxisAlignment: CrossAxisAlignment.start,
+                      //         children: <Widget>[
+                      //           ListTile(
+                      //             title: Text(
+                      //                 "You have an interview at Deloitte, scheduled on 2/04/2020"),
+                      //             trailing: Text("1 mo"),
+                      //           ),
+                      //           Padding(
+                      //               padding:
+                      //                   EdgeInsets.only(top: 20.0, left: 10.0),
+                      //               child: Container(
+                      //                 decoration: BoxDecoration(
+                      //                   border: Border.all(color: basicColor),
+                      //                   borderRadius: BorderRadius.circular(10),
+                      //                 ),
+                      //                 child: MaterialButton(
+                      //                     child: Text(
+                      //                       "View Application",
+                      //                       style: TextStyle(
+                      //                           fontSize: 18.0,
+                      //                           color: basicColor,
+                      //                           fontWeight: FontWeight.w600),
+                      //                     ),
+                      //                     onPressed: () => null),
+                      //               )),
+                      //         ]),
+                      //   ),
+                      // )
+                      );
+                }
               } else {
                 return Loading();
               }
