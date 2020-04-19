768e82efebee64ed02310b17fec37f977ad900d0
OjasKarmarkar <ojask2002@gmail.com>
2020-04-19 15:16:03 +0530

    Profile WIp

---
+++ lib/Screens/Home/Profile/editResume.dart
@@ -0,0 +1,54 @@
+import 'package:apli/Shared/constants.dart';
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+import 'package:flutter/material.dart';
+
+class EditResume extends StatefulWidget {
+  @override
+  _EditResumeState createState() => _EditResumeState();
+}
+
+class _EditResumeState extends State<EditResume> {
+
+  Widget resumeTile(String text){
+   return Padding(
+                 padding: const EdgeInsets.only(top:20.0),
+                 child: Container(
+              decoration: BoxDecoration(
+                  border:Border.all(color:Colors.grey.withOpacity(0.4))
+              ),
+              child: ListTile(title:Text(text , style: TextStyle(fontWeight:FontWeight.w600),) , trailing: IconButton(icon: Icon(EvaIcons.arrowIosForwardOutline), onPressed: null),)),
+               );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+          child: AppBar(
+            backgroundColor: basicColor,
+            automaticallyImplyLeading: true,
+            title: Text(
+              editResume,
+              style: TextStyle(color: Colors.white, fontSize: 24),
+            ),
+          ),
+          preferredSize: Size.fromHeight(70),
+        ),
+      body: SingleChildScrollView(
+        child:Padding(
+          padding:  EdgeInsets.only(top:80.0 , left: 20.0 , right: 20.0),
+          child: Column(children: <Widget>[
+           resumeTile(intro),
+           resumeTile(about),
+           resumeTile(experience),
+           resumeTile(education),
+           resumeTile(licenses),
+           resumeTile(skills),
+          ],
+          crossAxisAlignment: CrossAxisAlignment.start,
+          ),
+        )
+      ),
+    );
+  }
+}
--- lib/Screens/Home/Profile/profile.dart
+++ lib/Screens/Home/Profile/profile.dart
@@ -1,5 +1,7 @@
+import 'package:apli/Screens/Home/Profile/editResume.dart';
 import 'package:apli/Shared/constants.dart';
 import 'package:apli/Shared/customDrawer.dart';
+import 'package:apli/Shared/customTabBar.dart';
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 
@@ -8,7 +10,15 @@ class Profile extends StatefulWidget {
   _ProfileState createState() => _ProfileState();
 }
 
-class _ProfileState extends State<Profile> {
+class _ProfileState extends State<Profile> with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = new TabController(length: 3, vsync: this);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     final _scaffoldKey = GlobalKey<ScaffoldState>();
@@ -33,16 +43,158 @@ class _ProfileState extends State<Profile> {
               profile,
               style: TextStyle(color: Colors.white, fontSize: 24),
             ),
+            bottom: ColoredTabBar(
+                Colors.white,
+                TabBar(
+                  unselectedLabelColor: Colors.grey,
+                  labelColor: basicColor,
+                  tabs: [
+                    new Tab(
+                      text: resume,
+                    ),
+                    new Tab(
+                      text: videoIntro,
+                    ),
+                    new Tab(text: psychTest)
+                  ],
+                  controller: _tabController,
+                )),
           ),
-          preferredSize: Size.fromHeight(70),
+          preferredSize: Size.fromHeight(120),
         ),
-        body: SingleChildScrollView(
-          child: Column(children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(top: 300.0),
-              child: Center(child: Image.asset("Assets/Images/job.png")),
+        body: TabBarView(children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text(
+                    resumeSlogan,
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(left: 30.0),
+                      child: Image.asset("Assets/Images/job.png"),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: IconButton(
+                              icon: Icon(EvaIcons.editOutline),
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => EditResume()),
+                                );
+                              },
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: IconButton(
+                              icon: Icon(EvaIcons.shareOutline),
+                              onPressed: () {},
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: IconButton(
+                              icon: Icon(EvaIcons.downloadOutline),
+                              onPressed: () {},
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ],
             ),
-          ]),
-        ));
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text(
+                    videoIntroSlogan,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Align(child:Text("Instructions to follow" , style: TextStyle(fontSize:20 ,fontWeight: FontWeight.w600)) , alignment:Alignment.center),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Align(child:Text("1. Lorem Ipsum" , style: TextStyle(fontSize:20)), alignment:Alignment.center),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Align(child: Text("1. Lorem Ipsum" , style: TextStyle(fontSize:20)), alignment:Alignment.center),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Align(child: Text("1. Lorem Ipsum" ,style: TextStyle(fontSize:20)), alignment:Alignment.center),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Align(child: Text("1. Lorem Ipsum" , style: TextStyle(fontSize:20),), alignment:Alignment.center),
+                ),
+                Padding(
+                  padding:  EdgeInsets.only(top:40.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(color: basicColor),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: MaterialButton(
+                                child: Text(
+                                  "Upload From Gallery",
+                                  style: TextStyle(
+                                      fontSize: 18.0,
+                                      color: basicColor,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                onPressed: () => null),
+                          ),
+                      Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(color: basicColor),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: MaterialButton(
+                                child: Text(
+                                  "Record Now",
+                                  style: TextStyle(
+                                      fontSize: 18.0,
+                                      color: basicColor,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                onPressed: () => null),
+                          ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Center(child: Image.asset("Assets/Images/job.png")),
+        ], controller: _tabController));
   }
 }
