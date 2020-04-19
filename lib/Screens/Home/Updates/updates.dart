854dadd31cb5bfe3463e68f2cd89022699013b1f
harshgupta6789 <harshhvh999@gmail.com>
2020-04-19 18:17:23 +0530

    Updates done

--- lib/Screens/Home/Updates/updates.dart
+++ lib/Screens/Home/Updates/updates.dart
@@ -17,7 +17,6 @@ class Updates extends StatefulWidget {
 
 class _UpdatesState extends State<Updates> {
   Stream notifications;
-  FirebaseUser user;
   String batchID;
   String course;
   
@@ -26,20 +25,19 @@ class _UpdatesState extends State<Updates> {
   int currentTime = Timestamp.now().microsecondsSinceEpoch;
 
   userInit() async {
-    user = await FirebaseAuth.instance.currentUser();
 
-    Firestore.instance.collection('candidates').document(user.email)
-        .get().then((snapshot) =>
-    batchID = snapshot.data['batch_id']
-    );
-
-    Firestore.instance.collection('batches').document(batchID).
-    get().then((snapshot) =>
-    course = snapshot.data['course']
-    );
-
-    filters.add(user.email);
-    filters.add(course);
+//    Firestore.instance.collection('candidates').document(user.email)
+//        .get().then((snapshot) =>
+//    batchID = snapshot.data['batch_id']
+//    );
+//
+//    Firestore.instance.collection('batches').document(batchID).
+//    get().then((snapshot) =>
+//    course = snapshot.data['course']
+//    );
+//
+//    filters.add(user.email);
+//    filters.add(course);
   }
 
   @override
@@ -62,6 +60,7 @@ class _UpdatesState extends State<Updates> {
   Widget build(BuildContext context) {
     final _scaffoldKey = GlobalKey<ScaffoldState>();
     final user = Provider.of<User>(context);
+    filters.add(user.email);
     return Scaffold(
         key: _scaffoldKey,
         endDrawer: customDrawer(context, user),
@@ -93,149 +92,116 @@ class _UpdatesState extends State<Updates> {
           preferredSize: Size.fromHeight(70),
         ),
         body: StreamBuilder(
-            stream: notifications,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                print(filters);
-                snapshot.data.documents.forEach((f) {
-                  bool isMyNotification = false;
+          stream: Firestore.instance.collection('candidates').document(user.email).snapshots(),
+          builder: (context, snapshot1){
+            if(snapshot1.hasData) {
+              batchID = snapshot1.data['batch_id'];
+              return StreamBuilder(
+                stream: Firestore.instance.collection('batches').document(batchID).snapshots(),
+                builder: (context, snapshot2){
+                  if(snapshot2.hasData) {
+                    course = snapshot2.data['course'];
+                    filters.add(course);
+                    return StreamBuilder(
+                        stream: notifications,
+                        builder: (context, snapshot3) {
+                          if (snapshot3.hasData) {
+                            snapshot3.data.documents.forEach((f) {
+                              bool isMyNotification = false;
 
-                  String tempMessage = f.data['message'];
-                  Timestamp tempTime = f.data['time'];
-
-                  List receivers = f.data['receivers'];
-                  if (receivers != null) {
-                    for (int i = 0; i < filters.length; i++) {
-                      if (receivers.contains(filters[i])) {
-                        isMyNotification = true;
-                      }
-                      break;
-                    }
-                    if (isMyNotification) {
-                      if(tempTime != null) {
-                        myNotifications.add([
-                          tempMessage,
-                          difference(tempTime),
-                          f.documentID,
-                        ]);
-                      } else {
-                        myNotifications.add([
-                          tempMessage,
-                          null,
-                          f.documentID,
-                        ]);
-                      }
-                    }
-                  }
-                });
-                print(myNotifications);
-                if (myNotifications.length == 0) {
-                  return Center(
-                    child: Text('no new updates yet'),
-                  );
-                } else {
-                  return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ListView.builder(
-                        itemCount: myNotifications.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return Container(
-                            height: 180.0,
-                            child: Card(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(15.0),
-                              ),
-                              child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: <Widget>[
-                                    ListTile(
-                                      title: Text(myNotifications[index][0] ??
-                                          "No Message Exception"),
-                                      trailing: Text(myNotifications[index]
-                                              [1] ??
-                                          'No Time Exception'),
-                                    ),
-                                    Padding(
-                                        padding: EdgeInsets.only(
-                                            top: 20.0, left: 10.0),
-                                        child: Container(
-                                          decoration: BoxDecoration(
-                                            border:
-                                                Border.all(color: basicColor),
-                                            borderRadius:
-                                                BorderRadius.circular(10),
+                              String tempMessage = f.data['message'];
+                              Timestamp tempTime = f.data['time'];
+                              List receivers = f.data['receivers'];
+                              if (receivers != null) {
+                                for (int i = 0; i < receivers.length; i++) {
+                                  if (filters.contains(receivers[i])) {
+                                    isMyNotification = true;
+                                    break;
+                                  }
+                                }
+                                if (isMyNotification) {
+                                  if(tempTime != null) {
+                                    myNotifications.add([
+                                      tempMessage,
+                                      difference(tempTime),
+                                      f.documentID,
+                                    ]);
+                                  } else {
+                                    myNotifications.add([
+                                      tempMessage,
+                                      null,
+                                      f.documentID,
+                                    ]);
+                                  }
+                                }
+                              }
+                            });
+                            if (myNotifications.length == 0) {
+                              return Center(
+                                child: Text('no new updates yet'),
+                              );
+                            } else {
+                              return Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: ListView.builder(
+                                    itemCount: myNotifications.length,
+                                    itemBuilder: (BuildContext context, int index) {
+                                      return Container(
+                                        height: 180.0,
+                                        child: Card(
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(15.0),
                                           ),
-                                          child: MaterialButton(
-                                              child: Text(
-                                                "View Application",
-                                                style: TextStyle(
-                                                    fontSize: 18.0,
-                                                    color: basicColor,
-                                                    fontWeight:
-                                                        FontWeight.w600),
-                                              ),
-                                              onPressed: () => null),
-                                        )),
-                                  ]),
-                            ),
-                          );
-                        },
-                      )
-
-                      // Row(
-                      //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      //   children: <Widget>[
-                      //     RawChip(
-                      //       label: Text("New Jobs"),
-                      //       selected: false,
-                      //       backgroundColor: basicColor,
-                      //     ),
-                      //     RawChip(label: Text("Messages"), selected: true),
-                      //     RawChip(label: Text("Application"), selected: false),
-                      //   ],
-                      // ),
-
-                      //
-                      // Container(
-                      //   height: 180.0,
-                      //   child: Card(
-                      //     shape: RoundedRectangleBorder(
-                      //       borderRadius: BorderRadius.circular(15.0),
-                      //     ),
-                      //     child: Column(
-                      //         crossAxisAlignment: CrossAxisAlignment.start,
-                      //         children: <Widget>[
-                      //           ListTile(
-                      //             title: Text(
-                      //                 "You have an interview at Deloitte, scheduled on 2/04/2020"),
-                      //             trailing: Text("1 mo"),
-                      //           ),
-                      //           Padding(
-                      //               padding:
-                      //                   EdgeInsets.only(top: 20.0, left: 10.0),
-                      //               child: Container(
-                      //                 decoration: BoxDecoration(
-                      //                   border: Border.all(color: basicColor),
-                      //                   borderRadius: BorderRadius.circular(10),
-                      //                 ),
-                      //                 child: MaterialButton(
-                      //                     child: Text(
-                      //                       "View Application",
-                      //                       style: TextStyle(
-                      //                           fontSize: 18.0,
-                      //                           color: basicColor,
-                      //                           fontWeight: FontWeight.w600),
-                      //                     ),
-                      //                     onPressed: () => null),
-                      //               )),
-                      //         ]),
-                      //   ),
-                      // )
-                      );
+                                          child: Column(
+                                              crossAxisAlignment: CrossAxisAlignment.start,
+                                              children: <Widget>[
+                                                ListTile(
+                                                  title: Text(myNotifications[index][0] ??
+                                                      "No Message Exception"),
+                                                  trailing: Text(myNotifications[index]
+                                                  [1] ??
+                                                      'No Time Exception'),
+                                                ),
+                                                Padding(
+                                                    padding: EdgeInsets.only(
+                                                        top: 20.0, left: 10.0),
+                                                    child: Container(
+                                                      decoration: BoxDecoration(
+                                                        border:
+                                                        Border.all(color: basicColor),
+                                                        borderRadius:
+                                                        BorderRadius.circular(10),
+                                                      ),
+                                                      child: MaterialButton(
+                                                          child: Text(
+                                                            "View Application",
+                                                            style: TextStyle(
+                                                                fontSize: 18.0,
+                                                                color: basicColor,
+                                                                fontWeight:
+                                                                FontWeight.w600),
+                                                          ),
+                                                          onPressed: () => null),
+                                                    )),
+                                              ]),
+                                        ),
+                                      );
+                                    },
+                                  )
+                              );
+                            }
+                          } else {
+                            return Loading();
+                          }
+                        }
+                        );
+                  } else return Loading();
                 }
-              } else {
-                return Loading();
-              }
-            }));
+              );
+            } else {
+              return Loading();
+            }
+          },
+        ));
   }
 }
