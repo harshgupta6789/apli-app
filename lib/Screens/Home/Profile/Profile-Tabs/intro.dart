658a87fe1d0755e44ec292a54daf69ab0b5aafc1
OjasKarmarkar <ojask2002@gmail.com>
2020-05-02 22:26:59 +0530

    fix

--- lib/Screens/Home/Profile/Profile-Tabs/intro.dart
+++ lib/Screens/Home/Profile/Profile-Tabs/intro.dart
@@ -43,7 +43,7 @@ class _BasicIntroState extends State<BasicIntro> {
   }
 
   Future getImage() async {
-    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
 
     setState(() {
       _image = image;
