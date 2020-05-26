34141dd215dfe3e6b1d548460fe8d8f10a9a014e
harshgupta6789 <harshhvh999@gmail.com>
2020-05-26 22:45:04 +0530

    Common dart file for all three tabs

--- lib/Screens/Home/Jobs/jobTabs.dart
+++ lib/Screens/Home/Jobs/jobTabs.dart
@@ -30,8 +30,158 @@ class JobsTabs extends StatefulWidget {
 class _JobsTabssState extends State<JobsTabs> {
   double height, width, scale;
 
+  Widget deadlineToShow(String status, String deadlineTimer, String deadline) {
+    print(deadlineTimer);
+    switch (status) {
+      case "OFFERED":
+        return StreamBuilder(
+            stream: Stream.periodic(Duration(seconds: 1), (i) => i),
+            builder: (BuildContext context, AsyncSnapshot<int> snapshot2) {
+              // var dateString;
+
+              // int now = DateTime.now().millisecondsSinceEpoch;
+
+              // int estimateTs = deadlineTimer.millisecondsSinceEpoch;
+              // Duration remaining = Duration(milliseconds: estimateTs - now);
+              // if(remaining.isNegative)
+              //   dateString = 'Deadline is over';
+              // else if (remaining.inDays > 0) {
+              //   if (remaining.inDays == 1) {
+              //     dateString = remaining.inDays.toString() + ' day';
+              //   } else
+              //     dateString = remaining.inDays.toString() + ' days';
+              // } else {
+              //   if (remaining.inHours > 0) {
+              //     if (remaining.inHours == 1)
+              //       dateString = remaining.inHours.toString() + ' hour';
+              //     else
+              //       dateString = remaining.inHours.toString() + ' hours';
+              //   } else if (remaining.inMinutes > 0) if (remaining.inMinutes ==
+              //       1)
+              //     dateString = remaining.inMinutes.toString() + ' min';
+              //   else
+              //     dateString = remaining.inMinutes.toString() + ' mins';
+              //   else if (remaining.inSeconds == 1)
+              //     dateString = remaining.inSeconds.toString() + ' sec';
+              //   else
+              //     dateString = remaining.inSeconds.toString() + ' sec';
+              // }
+              return AutoSizeText(
+                "Deadline to accept : " + deadlineTimer,
+                maxLines: 2,
+                style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 12 * scale,
+                    fontWeight: FontWeight.w500),
+                overflow: TextOverflow.ellipsis,
+              );
+            });
+        break;
+
+      case "LETTER SENT":
+        return StreamBuilder(
+            stream: Stream.periodic(Duration(seconds: 1), (i) => i),
+            builder: (BuildContext context, AsyncSnapshot<int> snapshot2) {
+              var dateString;
+
+              // int now = DateTime.now().millisecondsSinceEpoch;
+
+              // int estimateTs = deadlineTimer.millisecondsSinceEpoch;
+              // Duration remaining = Duration(milliseconds: estimateTs - now);
+              // if(remaining.isNegative)
+              //   dateString = 'Deadline is over';
+              // else if (remaining.inDays > 0) {
+              //   if (remaining.inDays == 1) {
+              //     dateString = remaining.inDays.toString() + ' day';
+              //   } else
+              //     dateString = remaining.inDays.toString() + ' days';
+              // } else {
+              //   if (remaining.inHours > 0) {
+              //     if (remaining.inHours == 1)
+              //       dateString = remaining.inHours.toString() + ' hour';
+              //     else
+              //       dateString = remaining.inHours.toString() + ' hours';
+              //   } else if (remaining.inMinutes > 0) if (remaining.inMinutes ==
+              //       1)
+              //     dateString = remaining.inMinutes.toString() + ' min';
+              //   else
+              //     dateString = remaining.inMinutes.toString() + ' mins';
+              //   else if (remaining.inSeconds == 1)
+              //     dateString = remaining.inSeconds.toString() + ' sec';
+              //   else
+              //     dateString = remaining.inSeconds.toString() + ' sec';
+              // }
+
+              return AutoSizeText(
+                "Deadline to accept : " + deadlineTimer ??
+                    "No Deadline" + 'remaining',
+                maxLines: 2,
+                style: TextStyle(
+                    //color: Colors.red,
+                    fontSize: 12 * scale,
+                    fontWeight: FontWeight.w500),
+                overflow: TextOverflow.ellipsis,
+              );
+            });
+        break;
+
+      default:
+        return AutoSizeText(
+          'Deadline: ' +
+              dateToReadableTimeConverter(DateTime.parse(
+                  widget.jobs[0]['deadline'] ?? '2020-05-26 00:00:00')),
+          maxLines: 2,
+          style: TextStyle(
+              color: Colors.black,
+              fontSize: 12 * scale,
+              fontWeight: FontWeight.w500),
+          overflow: TextOverflow.ellipsis,
+        );
+    }
+  }
+
+  Widget differentBackground(String status) {
+    Color temp;
+    switch (status) {
+      case "OFFERED":
+        temp = Colors.green;
+        break;
+      case "INTERVIEW":
+        temp = Colors.blue;
+        break;
+      case "LETTER SENT":
+        temp = Colors.green;
+        break;
+      case "ACCEPTED":
+        temp = Colors.green;
+        break;
+      case "UNREVIEWED":
+        temp = Colors.grey;
+        break;
+      case "REJECTED":
+        temp = Colors.red;
+        break;
+      case "HIRED":
+        temp = Colors.green;
+        break;
+      default:
+        temp = Colors.green;
+    }
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(4.0),
+      child: Container(
+        color: temp,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(10, 4, 10, 4),
+          child: Text(status ?? "", style: TextStyle(color: Colors.white)),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    print((widget.jobs[0]['deadline']));
     width = MediaQuery.of(context).size.width;
     height = MediaQuery.of(context).size.height;
     if (width >= 360) {
@@ -40,161 +190,13 @@ class _JobsTabssState extends State<JobsTabs> {
       scale = 0.7;
     }
 
-    Widget deadlineToShow(
-        String status, String deadlineTimer, String deadline) {
-      print(deadlineTimer);
-      switch (status) {
-        case "OFFERED":
-          return StreamBuilder(
-              stream: Stream.periodic(Duration(seconds: 1), (i) => i),
-              builder: (BuildContext context, AsyncSnapshot<int> snapshot2) {
-                // var dateString;
-
-                // int now = DateTime.now().millisecondsSinceEpoch;
-
-                // int estimateTs = deadlineTimer.millisecondsSinceEpoch;
-                // Duration remaining = Duration(milliseconds: estimateTs - now);
-                // if(remaining.isNegative)
-                //   dateString = 'Deadline is over';
-                // else if (remaining.inDays > 0) {
-                //   if (remaining.inDays == 1) {
-                //     dateString = remaining.inDays.toString() + ' day';
-                //   } else
-                //     dateString = remaining.inDays.toString() + ' days';
-                // } else {
-                //   if (remaining.inHours > 0) {
-                //     if (remaining.inHours == 1)
-                //       dateString = remaining.inHours.toString() + ' hour';
-                //     else
-                //       dateString = remaining.inHours.toString() + ' hours';
-                //   } else if (remaining.inMinutes > 0) if (remaining.inMinutes ==
-                //       1)
-                //     dateString = remaining.inMinutes.toString() + ' min';
-                //   else
-                //     dateString = remaining.inMinutes.toString() + ' mins';
-                //   else if (remaining.inSeconds == 1)
-                //     dateString = remaining.inSeconds.toString() + ' sec';
-                //   else
-                //     dateString = remaining.inSeconds.toString() + ' sec';
-                // }
-                return AutoSizeText(
-                  "Deadline to accept : " + deadlineTimer,
-                  maxLines: 2,
-                  style: TextStyle(
-                      color: Colors.red,
-                      fontSize: 12 * scale,
-                      fontWeight: FontWeight.w500),
-                  overflow: TextOverflow.ellipsis,
-                );
-              });
-          break;
-
-        case "LETTER SENT":
-          return StreamBuilder(
-              stream: Stream.periodic(Duration(seconds: 1), (i) => i),
-              builder: (BuildContext context, AsyncSnapshot<int> snapshot2) {
-                var dateString;
-
-                // int now = DateTime.now().millisecondsSinceEpoch;
-
-                // int estimateTs = deadlineTimer.millisecondsSinceEpoch;
-                // Duration remaining = Duration(milliseconds: estimateTs - now);
-                // if(remaining.isNegative)
-                //   dateString = 'Deadline is over';
-                // else if (remaining.inDays > 0) {
-                //   if (remaining.inDays == 1) {
-                //     dateString = remaining.inDays.toString() + ' day';
-                //   } else
-                //     dateString = remaining.inDays.toString() + ' days';
-                // } else {
-                //   if (remaining.inHours > 0) {
-                //     if (remaining.inHours == 1)
-                //       dateString = remaining.inHours.toString() + ' hour';
-                //     else
-                //       dateString = remaining.inHours.toString() + ' hours';
-                //   } else if (remaining.inMinutes > 0) if (remaining.inMinutes ==
-                //       1)
-                //     dateString = remaining.inMinutes.toString() + ' min';
-                //   else
-                //     dateString = remaining.inMinutes.toString() + ' mins';
-                //   else if (remaining.inSeconds == 1)
-                //     dateString = remaining.inSeconds.toString() + ' sec';
-                //   else
-                //     dateString = remaining.inSeconds.toString() + ' sec';
-                // }
-
-                return AutoSizeText(
-                  "Deadline to accept : " + deadlineTimer ??
-                      "No Deadline" + 'remaining',
-                  maxLines: 2,
-                  style: TextStyle(
-                      //color: Colors.red,
-                      fontSize: 12 * scale,
-                      fontWeight: FontWeight.w500),
-                  overflow: TextOverflow.ellipsis,
-                );
-              });
-          break;
-
-        default:
-          return AutoSizeText(
-            'Deadline: ' + deadline ?? "No Deadline",
-            maxLines: 2,
-            style: TextStyle(
-                color: Colors.black,
-                fontSize: 12 * scale,
-                fontWeight: FontWeight.w500),
-            overflow: TextOverflow.ellipsis,
-          );
-      }
-    }
-
-    Widget differentBackground(String status) {
-      Color temp;
-      switch (status) {
-        case "OFFERED":
-          temp = Colors.green;
-          break;
-        case "INTERVIEW":
-          temp = Colors.blue;
-          break;
-        case "LETTER SENT":
-          temp = Colors.green;
-          break;
-        case "ACCEPTED":
-          temp = Colors.green;
-          break;
-        case "UNREVIEWED":
-          temp = Colors.grey;
-          break;
-        case "REJECTED":
-          temp = Colors.red;
-          break;
-        case "HIRED":
-          temp = Colors.green;
-          break;
-        default:
-          temp = Colors.green;
-      }
-      return ClipRRect(
-        borderRadius: BorderRadius.circular(4.0),
-        child: Container(
-          color: temp,
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(10, 4, 10, 4),
-            child: Text(status ?? "", style: TextStyle(color: Colors.white)),
-          ),
-        ),
-      );
-    }
-
     if ((widget.jobs ?? []).length == 0) {
       return Center(
           child: Text(widget.tabNo == 0
               ? 'You have not applied for any jobs yet'
               : widget.tabNo == 1
-                  ? "There are no new jobs"
-                  : "You don't have any incomplete jobs"));
+              ? "There are no new jobs"
+              : "You don't have any incomplete jobs"));
     } else
       return ScrollConfiguration(
         behavior: MyBehavior(),
@@ -233,7 +235,7 @@ class _JobsTabssState extends State<JobsTabs> {
                                                           status: widget
                                                               .profileStatus,
                                                           st: widget.jobs[index]
-                                                              ['status'],
+                                                          ['status'],
                                                         )));
                                           else if (widget.tabNo == 1)
                                             Navigator.push(
@@ -250,7 +252,7 @@ class _JobsTabssState extends State<JobsTabs> {
                                                         )));
                                           else {
                                             if (widget.alreadyAccepted !=
-                                                    null &&
+                                                null &&
                                                 widget.alreadyAccepted) {
                                               showToast(
                                                   "You have already accepted a job",
@@ -281,9 +283,9 @@ class _JobsTabssState extends State<JobsTabs> {
                                         ),
                                         subtitle: Column(
                                           mainAxisAlignment:
-                                              MainAxisAlignment.start,
+                                          MainAxisAlignment.start,
                                           crossAxisAlignment:
-                                              CrossAxisAlignment.start,
+                                          CrossAxisAlignment.start,
                                           children: [
                                             AutoSizeText(
                                               widget.jobs[index]['location'] ??
@@ -297,39 +299,40 @@ class _JobsTabssState extends State<JobsTabs> {
                                             ),
                                             widget.tabNo == 0
                                                 ? deadlineToShow(
-                                                    widget.jobs[index]
-                                                        ['status'],
-                                                    widget.jobs[index][
-                                                            'accept_deadline'] ??
-                                                        '',
-                                                    widget.jobs[index]
-                                                            ['deadline'] ??
-                                                        "")
+                                                widget.jobs[index]
+                                                ['status'],
+                                                widget.jobs[index][
+                                                'accept_deadline'] ??
+                                                    '',
+                                                widget.jobs[index]
+                                                ['deadline'] ??
+                                                    "")
                                                 : AutoSizeText(
-                                                    'Deadline: ' +
-                                                            widget.jobs[index]
-                                                                ['deadline'] ??
-                                                        "No Deadline",
-                                                    maxLines: 2,
-                                                    style: TextStyle(
-                                                        color: Colors.black,
-                                                        fontSize: 12 * scale,
-                                                        fontWeight:
-                                                            FontWeight.w500),
-                                                    overflow:
-                                                        TextOverflow.ellipsis,
-                                                  )
+                                              'Deadline: ' +
+                                                  dateToReadableTimeConverter(
+                                                      DateTime.parse(widget
+                                                          .jobs[0]['deadline'] ??
+                                                          '2020-05-26 00:00:00')),
+                                              maxLines: 2,
+                                              style: TextStyle(
+                                                  color: Colors.black,
+                                                  fontSize: 12 * scale,
+                                                  fontWeight:
+                                                  FontWeight.w500),
+                                              overflow:
+                                              TextOverflow.ellipsis,
+                                            )
                                           ],
                                         ),
                                         trailing: widget.tabNo == 0
                                             ? differentBackground(
-                                                widget.jobs[index]['status'])
+                                            widget.jobs[index]['status'])
                                             : IconButton(
-                                                icon: Icon(
-                                                  EvaIcons.bookmarkOutline,
-                                                ),
-                                                onPressed: () async {},
-                                              )),
+                                          icon: Icon(
+                                            EvaIcons.bookmarkOutline,
+                                          ),
+                                          onPressed: () async {},
+                                        )),
                                   ),
                                 ])),
                       );
